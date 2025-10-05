731c34dbffa76394c9c10c6bb5d791951ad64f55
kimmerr9 <justin.kim2665@hotmail.com>
2025-10-05 18:36:33 -0400

    small changes

--- interrupts.cpp
+++ interrupts.cpp
@@ -26,12 +26,14 @@ int main(int argc, char** argv) {
     int ISR_COST = 40; //time to execute ISR
     int VECTOR_LOOKUP = 1; //time to lookup vector address
     int ISR_ADDR_LOOKUP = 1; //time to lookup ISR address
+    int DATA_TRANSFER = 40; // time to transfer data from device to memory
 
-    const int DEVICE_COUNT = 32; //number of devices
-    int device_due_at[DEVICE_COUNT]; //array to track when each device will complete its I/O
-    for (int i = 0; i < DEVICE_COUNT; i++) {
-        device_due_at[i] = -1; //initialize all devices to not busy
-    }
+    //refactored: oct4 2025 10:02PM
+    // const int DEVICE_COUNT = 32; //number of devices
+    // int device_due_at[DEVICE_COUNT]; //array to track when each device will complete its I/O
+    // for (int i = 0; i < DEVICE_COUNT; i++) {
+    //     device_due_at[i] = -1; //initialize all devices to not busy
+    // }
 
 
 
@@ -58,25 +60,30 @@ int main(int argc, char** argv) {
             execution += std::to_string(now_ms) + ", " + std::to_string(VECTOR_LOOKUP) + ", find vector " + std::to_string(duration_intr) + " in memory position " + std::to_string(mem_pos) + "\n";
             now_ms += VECTOR_LOOKUP;
 
-            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_ADDR_LOOKUP) + ", obtain ISR address (" + vectors.at(duration_intr) + ")\n";
+            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_ADDR_LOOKUP) + ", load ISR address (" + vectors.at(duration_intr) + ") into the PC\n";
             now_ms += ISR_ADDR_LOOKUP;
 
-            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_COST) + ", call device driver\n";
+            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_COST) + ", SYSCALL: run the ISR\n";
             now_ms += ISR_COST;
 
-            device_due_at[duration_intr] = now_ms + delays.at(duration_intr);
+            execution += std::to_string(now_ms) + ", " + std::to_string(DATA_TRANSFER) + ", transfer data from device to memory\n";
+            now_ms += DATA_TRANSFER;
+
+            int device_delay = delays.at(device); // get device number X's delay time 
+            execution += std::to_string(now_ms) + ", " + std::to_string(device_delay) + ", check for errors\n";
+            now_ms += device_delay;
 
             execution += std::to_string(now_ms) + ", " + std::to_string(KERNEL_MODE) + ", IRET\n";
             now_ms += KERNEL_MODE;
         }
         else if (activity == "END_IO"){
-            if(duration_intr >=0 && duration_intr < DEVICE_COUNT && device_due_at[duration_intr]>=0 && now_ms<device_due_at[duration_intr]){
-                now_ms = device_due_at[duration_intr];
-            }
+            // if(duration_intr >=0 && duration_intr < DEVICE_COUNT && device_due_at[duration_intr]>=0 && now_ms<device_due_at[duration_intr]){
+            //     now_ms = device_due_at[duration_intr];
+            // }
 
-            if (duration_intr>=0 && duration_intr < DEVICE_COUNT){
-                device_due_at[duration_intr] = -1; //reset device to not busy
-            }
+            // if (duration_intr>=0 && duration_intr < DEVICE_COUNT){
+            //     device_due_at[duration_intr] = -1; //reset device to not busy
+            // }
             execution += std::to_string(now_ms) + ", " + std::to_string(KERNEL_MODE) + ", switch to kernel mode\n";
             now_ms += KERNEL_MODE;
 
@@ -87,11 +94,15 @@ int main(int argc, char** argv) {
             execution += std::to_string(now_ms) + ", " + std::to_string(VECTOR_LOOKUP) + ", find vector " + std::to_string(duration_intr) + " in memory position " + std::to_string(mem_pos) + "\n";
             now_ms += VECTOR_LOOKUP;
 
-            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_ADDR_LOOKUP) + ", obtain ISR address (" + vectors.at(duration_intr) + ")\n";
+            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_ADDR_LOOKUP) + ", obtain ISR address (" + vectors.at(duration_intr) + ") into the PC\n";
             now_ms += ISR_ADDR_LOOKUP;
 
-            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_COST) + ", end of I/O " + std::to_string(duration_intr) + ": interrupt service\n";
+            execution += std::to_string(now_ms) + ", " + std::to_string(ISR_COST) + ", ENDIO: run the ISR\n";
             now_ms += ISR_COST;
+
+            int device_delay = delays.at(device); // get device number X's delay time 
+            execution += std::to_string(now_ms) + ", " + std::to_string(device_delay) + ", check for errors\n";
+            now_ms += device_delay;
 
             execution += std::to_string(now_ms) + ", " + std::to_string(KERNEL_MODE) + ", IRET\n";
             now_ms += KERNEL_MODE;
